c8ff2d20067118d970ddd8acc973d0eedb905505
James Chandler <james.chandler@trace3.com>
2022-05-13 11:30:16 -0700

    move workspace token to gh secrets

--- .github/workflows/workspace.tf
+++ .github/workflows/workspace.tf
@@ -5,6 +5,5 @@ terraform {
     workspaces {
       name = "tfcloud-actionstest"
     }
-    token = "uATFRgZxInvm6A.atlasv1.yaqyBNwyyyKrqnApDpGc5NILcPIdWYClJj71wdTCCPBpxhmygpSrjqtiGnWChby9cUw"
   }
 }
